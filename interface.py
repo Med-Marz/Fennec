c5ed2308577509336b8fd6def44d2db5f4a81ac1
med-marz <med-marz@med-marz-1-0>
2024-05-17 11:23:51 +0100

    add loading indicator while generating response

--- interface.py
+++ interface.py
@@ -1,15 +1,18 @@
-from PyQt5 import QtCore, QtWidgets
+from PyQt5 import QtCore, QtWidgets, uic
 import sys
 from threading import Thread
 from assistant import generate_text_response, text_to_speech
 
 
-class Ui_MainWindow(object):
+class Ui_MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         self.thread = None
 
     def setupUi(self, MainWindow):
+        # Load UI file
+        uic.loadUi("interface.ui", self)
+
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(800, 600)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
@@ -17,6 +20,7 @@ class Ui_MainWindow(object):
         self.groupBox = QtWidgets.QGroupBox(self.centralwidget)
         self.groupBox.setGeometry(QtCore.QRect(140, 60, 321, 401))
         self.groupBox.setObjectName("groupBox")
+        self.groupBox.setStyleSheet("background-color: rgb(61, 56, 70);")
         self.scrollArea = QtWidgets.QScrollArea(self.groupBox)
         self.scrollArea.setGeometry(QtCore.QRect(0, 20, 321, 351))
         self.scrollArea.setWidgetResizable(True)
@@ -25,19 +29,18 @@ class Ui_MainWindow(object):
         self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 319, 349))
         self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
         self.scrollArea.setWidget(self.scrollAreaWidgetContents)
-        self.textEdit = QtWidgets.QTextEdit(
-            self.scrollAreaWidgetContents
-        )  # Add textEdit attribute
-        self.textEdit.setGeometry(
-            QtCore.QRect(0, 0, 321, 351)
-        )  # Add textEdit attribute
-        self.textEdit.setObjectName("textEdit")  # Add textEdit attribute
+        self.textEdit = QtWidgets.QTextEdit(self.scrollAreaWidgetContents)
+        self.textEdit.setGeometry(QtCore.QRect(0, 0, 321, 351))
+        self.textEdit.setObjectName("textEdit")
+        self.textEdit.setStyleSheet("background-color: rgb(61, 56, 70); color: white;")
         self.lineEdit = QtWidgets.QLineEdit(self.groupBox)
         self.lineEdit.setGeometry(QtCore.QRect(0, 370, 231, 31))
         self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit.setStyleSheet("background-color: rgb(94, 92, 100);")
         self.pushButton = QtWidgets.QPushButton(self.groupBox)
         self.pushButton.setGeometry(QtCore.QRect(230, 370, 91, 31))
         self.pushButton.setObjectName("pushButton")
+        self.pushButton.setStyleSheet("color: white;")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 22))
@@ -69,21 +72,17 @@ class Ui_MainWindow(object):
 
     def process_response(self, user_input):
         # Generate response
-        response = generate_text_response(user_input)
 
         # Display response in text edit widget
-        QtCore.QMetaObject.invokeMethod(
-            self.textEdit,
-            "append",
-            QtCore.Qt.QueuedConnection,
-            QtCore.Q_ARG(str, "You: " + user_input),
-        )
-        QtCore.QMetaObject.invokeMethod(
-            self.textEdit,
-            "append",
-            QtCore.Qt.QueuedConnection,
-            QtCore.Q_ARG(str, "Assistant: " + response),
-        )
+        self.textEdit.append("You: " + user_input)
+        self.textEdit.append("Assistant: Loading...")
+
+        #
+        response = generate_text_response(user_input)
+        # Clear textEdit content - clear loading
+        self.textEdit.undo()
+        self.textEdit.append("Assistant: " + response)
+
         self.lineEdit.clear()
 
         # Call text_to_speech function to convert response to speech
